e5eabe47d9758fa21ef5711c2bdfe609aeb5c4ca
sollankie <ignat.pauliuchenka@yandex.ru>
2022-11-22 00:50:19 +0300

    feat: Added snake and Canvas, Now snake can eat and grow, change its location

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,121 @@
 <template>
-  <div>
-    <div><p>Snake game</p></div>
-    
-  </div>
+<div id="main_page">
+  <h1>Snake game</h1>
+  <canvas id="board"></canvas>
+
+</div>
 </template>
 
 <script>
-
-
 export default {
+}
+let blockSize = 25;
+let rows = 20;
+let cols = 20;
+let board;
+let context;
 
+//snake
+let SnakeX = blockSize * 5;
+let SnakeY = blockSize * 5;
+
+let velocityX = 0;
+let velocityY = 0;
+
+//snakebody
+let snakeBody = []
+
+//food
+let foodX;
+let foodY;
+
+let gameOver = false;
+window.onload = function() {
+  board = document.getElementById("board");
+  board.height = rows * blockSize;
+  board.width = cols * blockSize; 
+  context = board.getContext("2d");
+
+
+  placefood();
+  document.addEventListener("keyup", changeDirection);
+  // update();
+  setInterval(update, 1000/10);
+}
+
+function update() {
+  if (gameOver) {
+    return;
+  }
+  context.fillStyle="black";
+  context.fillRect(0, 0 , board.width, board.height);
+
+  context.fillStyle="red"
+  context.fillRect(foodX, foodY, blockSize, blockSize);
+
+  if (SnakeX == foodX && SnakeY == foodY) {
+    snakeBody.push([foodX, foodY])
+    placefood();
+  }
+
+  for (let i = snakeBody.length-1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i-1];
+
+  }
+  if (snakeBody.length) {
+    snakeBody[0] = [SnakeX, SnakeY]
+  }
+
+  context.fillStyle="lime";
+  SnakeX += velocityX * blockSize;
+  SnakeY += velocityY * blockSize;
+  context.fillRect(SnakeX, SnakeY, blockSize, blockSize);
+  for (let i = 0; i , snakeBody.length; i++) {
+    context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+  }
+}
+//game over conditions
+ if (SnakeX < 0 || SnakeX > cols*blockSize || SnakeY < 0 || SnakeY > rows*blockSize ) {
+   gameOver = true;
+   alert("Game Over");
+ }
+
+ for (let i = 0; i < snakeBody.length; i++) {
+   if (SnakeX == snakeBody[i][0] && SnakeY == snakeBody[i][1]) {
+     gameOver = true;
+     alert("Game Over");
+   }
+ }
+
+function changeDirection(e) {
+  if (e.code == "ArrowUp" && velocityY != 1) {
+    velocityX = 0;
+    velocityY = -1;
+  }
+  else if (e.code == "ArrowDown" && velocityY != -1) {
+    velocityX = 0;
+    velocityY = 1;
+  }
+  else if (e.code == "ArrowLeft" && velocityX != 1) {
+    velocityX = -1;
+    velocityY = 0;
+  }
+  if (e.code == "ArrowRight" && velocityX != -1) {
+    velocityX = 1;
+    velocityY = 0;
+  }
+
+}
+
+function placefood() {
+  foodX = Math.floor(Math.random() * cols)* blockSize;
+  foodY = Math.floor(Math.random() * rows) * blockSize;
 }
 </script>
 
 <style>
-
+  body {
+    font-family: 'Courier New', Courier, monospace;
+    text-align: center;
+  }
 </style>
